ed3893601875167e2098d65a8ff18e260270b15e
Pham trung Hieu <80968463+0988660682hieu@users.noreply.github.com>
2023-02-09 15:16:15 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         new Quiz();
-    }
+        BobbieStrickland7676@chedoden.com 
+        pass 
+    }43282211
 }
